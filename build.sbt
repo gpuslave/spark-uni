40ed8f6a9f8bf2bc965c35ac75124d445fe2730d
alex <shimanoee@yandex.ru>
2025-04-20 14:53:13 +0300

    update

--- build.sbt
+++ build.sbt
@@ -1,7 +1,21 @@
 name := "App"
 
-version := "0.1"
+version := "1"
 
-scalaVersion := "2.12.18"
+scalaVersion := "2.12.20"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.5"
+
+// 1. Declare the main class to embed in the manifest
+assembly / mainClass := Some("App")
+
+// 2. (Optional) Customize the output JAR name
+assembly / assemblyJarName := "app-assembly.jar"
+
+// 3. Handle META‑INF merge conflicts
+import sbtassembly.AssemblyPlugin.autoImport._
+import sbtassembly.MergeStrategy
+assembly / assemblyMergeStrategy := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case _                             => MergeStrategy.first
+}
--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -2,7 +2,8 @@ import org.apache.spark.sql.SparkSession
 
 object App {
   def main(args: Array[String]): Unit = {
-    val logFile = "/opt/spark/work-dir/src/TEST.txt"
+    val logFile = "/app/src/TEST.txt"
+    // val logFile = "/opt/spark/work-dir/src/TEST.txt"
     val spark = SparkSession
       .builder
       .appName("App")
@@ -24,6 +25,21 @@ object App {
       .count()
 
     println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+    spark.stop()
+  }
+}
+
+object Aboba {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("App")
+      .getOrCreate()
+    
+    spark.sparkContext.setLogLevel("ERROR")
+    
+    println(s"ABOBA")
 
     spark.stop()
   }
